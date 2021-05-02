b1c7ae260b95e7bbda49b7df2745cc31fc8198de
Ilmir <78535469+root0f@users.noreply.github.com>
2021-05-02 18:18:14 +0300

    Connected firebase, request to server working correctly.

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => (
+    <div className="jumbotron">
+        <div className="container">
+            <h1 className="display-4">Обычное приложение на React</h1>
+            <p className="lead">Версия приложения <strong>1.42.5</strong></p>
+        </div>
+    </div>
+)
+
